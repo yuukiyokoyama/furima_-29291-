e4f8276f6587c68c7ad8ed5a6570fc5ca8528532
yuukiyokoyama <yuukitigars89@gmail.com>
2020-08-26 08:57:37 +0900

    deviseに関するビューファイルの配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :basic_auth
+  before_action :authenticate_user!
 
   private
 
