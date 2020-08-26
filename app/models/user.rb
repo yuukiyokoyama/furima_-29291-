7000486e430e9b5953316461d593f99c48dfe4e7
yuukiyokoyama <yuukitigars89@gmail.com>
2020-08-26 17:01:32 +0900

    ログイン完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  # validates :nickname, presence: true
+  validates :nickname, presence: true
   
-  # validates :password, uniqueness: true
-  #                      presence: true
-  #                      length: { minimum: 6 }
-  #                      format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,/^[a-zA-Z0-9]+$/
-  #                    }
+  validates :password, presence: true
+                       length: { minimum: 6 }
+                       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,/^[a-zA-Z0-9]+$/ }
 
-  # validates :family_name, presence: true
-  #                      format: { with:  /^[ぁ-んァ-ヶー一-龠]+$/ }
-  #           :first_name,  presence: true 
-  #                         format: { with:  /^[ぁ-んァ-ヶー一-龠]+$/ }
-  #           :family_name_kana, presence: true
-  #                              format: { with:/^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/ }
-  #           :first_name_kana, presence: true
-  #                             format: { with:/^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/ }
-  #           :birthday, presence: true                                     
+  validates :family_name, presence: true
+                       format: { with:  /^[ぁ-んァ-ヶー一-龠]+$/ }
+            :first_name,  presence: true 
+                          format: { with:  /^[ぁ-んァ-ヶー一-龠]+$/ }
+            :family_name_kana, presence: true
+                               format: { with:/^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/ }
+            :first_name_kana, presence: true
+                              format: { with:/^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/ }
+            :birthday, presence: true                                     
 end
