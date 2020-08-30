6ff60003aaa31b7892704ac2a5ac56fb8e4d7e09
yuukiyokoyama <yuukitigars89@gmail.com>
2020-08-30 19:40:00 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,2 @@
 class PostsController < ApplicationController
-#   def index
-#   end
-
-#   def new
-#   end
-
-#   def create
-#     Post.create(post_params)
-#   end
-
-#   private
-
-#   def ppost_params
-#     params.require(:post).permit(:name, :image, :text)
-#   end
-# end
+end
