8757907ec85a387cc13c7e02eaa0e58c85c52faa
yuukiyokoyama <yuukitigars89@gmail.com>
2020-09-01 19:29:40 +0900

    商品削除機能修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,14 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destroy
-    redirect_to root_path if @item.destroy
-end
+    def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end  
+  end
+  
 
   private
 
