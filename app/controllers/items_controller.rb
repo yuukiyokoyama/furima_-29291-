e5574604c6277cb3448cfce5ce6b7c32c325832e
yuukiyokoyama <yuukitigars89@gmail.com>
2020-09-01 18:50:03 +0900

    商品削除機能実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -26,6 +26,10 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    redirect_to root_path if @item.destroy
+end
 
   private
 
